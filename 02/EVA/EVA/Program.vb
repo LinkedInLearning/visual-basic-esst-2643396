33c09e2b4df012ac2d2a34d8be4a26e58d6e4bfb
Ralph Steyer <ralph.steyer@rjs.de>
2023-10-12 09:46:08 +0200

    Kapitel 2

---
+++ 02/EVA/EVA/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim name As String
+
+        Console.WriteLine("Geben Sie Ihren Namen an")
+        name = Console.ReadLine()
+        Console.WriteLine("Hallo {0}. Viel Spass bei der Arbeit!", name)
+
+    End Sub
+End Module
